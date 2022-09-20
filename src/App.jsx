fbaac983c6efcbab9c6959f29297a0cb68b544d2
AnnaNychyk <annanichichka@gmail.com>
2022-09-20 16:44:47 +0300

    done hw without styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import Navbar from './components/Navbar/Navbar';
 import Register from 'pages/Register';
 import Login from 'pages/Login';
 import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
 import PublicRoute from 'components/PublicRoute/PublicRoute';
+import { current } from 'redux/auth/auth-operations';
 
 const Phonebook = lazy(() => import('./components/Phonebook/Phonebook'));
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(current());
+  }, [dispatch]);
+
   return (
     <>
       <Navbar />
