bc937d760cdf18e13a108b34e017722218a98032
AnnaNychyk <annanichichka@gmail.com>
2022-09-20 14:13:13 +0300

    add logout

--- src/components/Navbar/NavbarUser/NavbarUser.jsx
+++ src/components/Navbar/NavbarUser/NavbarUser.jsx
@@ -1,13 +1,23 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { getUser } from 'redux/auth/auth-selectors';
 import styles from './NavbarUser.module.css';
+import { logout } from 'redux/auth/auth-operations';
 
 const NavbarUser = () => {
+  const dispatch = useDispatch();
   const { name } = useSelector(getUser);
+
+  const onLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <div className={styles.wrapper}>
-      <span>{name}</span>|<button type="button">logout</button>
+      <span>{name}</span>|
+      <button onClick={onLogout} type="button">
+        logout
+      </button>
     </div>
   );
 };
